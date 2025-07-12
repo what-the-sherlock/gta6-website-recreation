388ee0c0de240cae4020e82f6968b060fc397284
what-the-sherlock <aashu0333@gmail.com>
2025-07-13 02:09:47 +0530

    implement lucia video

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import { ScrollTrigger } from 'gsap/all'
 import Hero from './sections/Hero';
 import NavBar from './sections/NavBar';
 import FirstVideo from './sections/FirstVideo';
+import Jason from './sections/Jason';
+import SecondVideo from './sections/SecondVideo';
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -14,6 +16,8 @@ function App() {
       <NavBar/>
       <Hero/>
       <FirstVideo/>
+      <Jason/>
+      <SecondVideo/>
     </main>
   )
 }
